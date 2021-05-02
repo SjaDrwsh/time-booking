d946fabc7002f95b25df76d326e10aa977fa7ddd
sdr <sdr@knowis.de>
2021-05-02 16:38:29 +0200

    Group by date not day

--- src/util/dateUtil.ts
+++ src/util/dateUtil.ts
@@ -2,27 +2,39 @@ import { TimeSlotData } from "interfaces/response";
 
 // array to map name of the day and sunday is day 0 
 export const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+
+// the key would be the day in number form and each object would contain the time slots available that day
 export interface groupedDates {
     [key: string]: SelectedDates[]
 }
 
+// extending the data to contain selected flag for the UI handling
 export interface SelectedDates extends TimeSlotData{
     selected?: boolean;
 }
 
+/**
+ * groups the array of time slots into days object and sorts the array of time
+ * @param timeSlotsArray 
+ * @returns groupedDates an object days containing an array of time slot for that day
+ */
 export function groupByDay(timeSlotsArray: SelectedDates[]): groupedDates{
     
+    // creating a hash map of days as keys to group by day
     const daysObject: groupedDates = {};
 
     for (const iterator of timeSlotsArray) {
         const startDate = new Date(iterator.start_time);
-        const day = days[startDate.getDay()];
-        if(!daysObject[day]){
-            daysObject[day] = [];
+         const date = startDate.getFullYear()+'-'+(startDate.getMonth()+1)+'-'+startDate.getDate();
+        console.log(startDate.getFullYear()+'-'+(startDate.getMonth()+1)+'-'+startDate.getDate())
+        // check if day deos not exists added it days object then push time slots
+        if(!daysObject[date]){
+            daysObject[date] = [];
         }
-        daysObject[day].push({start_time: iterator.start_time, end_time: iterator.end_time, selected: iterator.selected});
+        daysObject[date].push({start_time: iterator.start_time, end_time: iterator.end_time, selected: iterator.selected});
     }
     
+    // calling the sort function on every day of the week
     for (const key in daysObject) {
         daysObject[key] = sortDates(daysObject[key]);
     }
@@ -30,17 +42,33 @@ export function groupByDay(timeSlotsArray: SelectedDates[]): groupedDates{
     return daysObject;
 }
 
+/**
+ * sort array by start time end_time
+ * @param dates to sort 
+ * @returns a sorted array of type TimeSlotData
+ */
 export function sortDates(dates: TimeSlotData[]): TimeSlotData[]{
     return dates.sort(function(a, b) {
-        return (a.start_time < b.start_time) ? -1 : ((a.start_time > b.start_time) ? 1 : 0);
+        return (a.end_time < b.end_time) ? -1 : ((a.end_time > b.end_time) ? 1 : 0);
     });
 } 
 
+/**
+ * converts ISO format time to human readable time to display on UI
+ * @param date 
+ * @returns formatted date
+ */
 export function convertDateToReadableFormat(date: string): string{
     const newDate = new Date(date);
     return `${newDate.getHours()}:${newDate.getMinutes()}`;
 }
 
+/**
+ * adding flag selected to time, so only allow one company per time slot
+ * @param availableTimeSlots 
+ * @param selectedDates 
+ * @returns grouped and sorted array
+ */
 export function mapSelectedTimeSlots(availableTimeSlots: SelectedDates[], selectedDates: groupedDates | undefined): groupedDates{
     if (!selectedDates || Object.keys(selectedDates).length === 0){
         return groupByDay(availableTimeSlots);
@@ -56,6 +84,7 @@ export function mapSelectedTimeSlots(availableTimeSlots: SelectedDates[], select
                 availableTimeSlot.selected = true;
                 continue;
             } 
+            availableTimeSlot.selected = false;
         }   
     }
     
